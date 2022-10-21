d573072cdc49461302bed9d8b0133c2adcf47f1b
EMMANUEL <hemazyn@gmail.com>
2022-10-21 04:36:56 +0100

    working on js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+/** NOTE LIGHT & DARK MODE **/
 var moon = document.getElementById("moon");
-
 moon.onclick = function () {
     document.body.classList.toggle("theme-mode");
     if (document.body.classList.contains("theme-mode")) {
@@ -9,11 +9,59 @@ moon.onclick = function () {
     }
 }
 
-
-// ACTIVE SECTION
-$(function () {
-    $('.navbar-nav li a').click(function () {
-        $('.navbar-nav li').removeClass();
-        $($(this).attr('href')).addClass('active');
+/** NOTE  active nav-link section **/
+var sections = document.querySelectorAll("section");
+var navLink = document.querySelectorAll(".nav-link");
+window.onscroll = () => {
+    let current = "";
+    sections.forEach((section) => {
+        var sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 60) {
+            current = section.getAttribute("id");
+        }
     });
-});
+    /** NOTE nav-link **/
+    navLink.forEach((a) => {
+        a.classList.remove("active");
+        if (a.href.includes(current)) {
+            a.classList.add("active");
+        }
+    });
+};
+
+/** NOTE  ANIMATE ON SCROLL **/
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
+
+/** NOTE  active nav-link section **/
+// var sections = document.querySelectorAll("section");
+// var navLink = document.querySelectorAll(".nav-link");
+// window.onscroll = () => {
+//     let current = "";
+//     sections.forEach((section) => {
+//         var sectionTop = section.offsetTop;
+//         if (pageYOffset >= sectionTop - 60) {
+//             current = section.getAttribute("id");
+//         }
+//     });
+//     /** NOTE nav-link **/
+//     navLink.forEach((a) => {
+//         a.classList.remove("active");
+//         if (a.href.includes(current)) {
+//             a.classList.add("active");
+//         }
+//     });
+// };
